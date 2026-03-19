b6b732eefe2da417e246e58b43d33e2dceae1eac
Sviatoslav Titov <titovsvatoslav@gmail.com>
2026-03-19 12:20:52 +0400

    - Update BGFetching

--- task_flow/task_flow/task_flowApp.swift
+++ task_flow/task_flow/task_flowApp.swift
@@ -1,5 +1,7 @@
+import BackgroundTasks
 import SwiftData
 import SwiftUI
+import os
 
 class Navigator: NavigatorProtocol {
     @Published var path: NavigationPath = .init()
@@ -9,12 +11,36 @@ class Navigator: NavigatorProtocol {
 struct task_flowApp: App {
     @Environment(\.network) var network
     @State private var source = TaskSourceViewModel()
+    @Environment(\.modelContext) var context
 
     var body: some Scene {
         WindowGroup {
             NavigatorView(coordinator: Navigator())
                 .modelContainer(for: TaskEntity.self)
                 .environment(source)
+                .onAppear {
+                    // TODO: Move bg service
+                    BGTaskScheduler.shared.register(
+                        forTaskWithIdentifier: "com.testapp.title", using: nil
+                    ) { task in
+                        guard let task = task as? BGProcessingTask else { return }
+
+                        let syncServcice = SyncService(
+                            storage: PersistentRepository<TaskEntity, TaskModel>(
+                                modelContainer: context.container),
+                            network: network
+                        )
+
+                        Task {
+                            await syncServcice.start()
+                            task.setTaskCompleted(success: true)
+                        }
+                    }
+                }
         }
+    }
+
+    func handleProcessingTask(task: BGProcessingTask) {
+
     }
 }
